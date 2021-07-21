732da4d200e34a90c3d8c1e3cd0eaf4d1d14d0fe
mostafa.drz <mostafa69d@gmail.com>
2021-07-21 13:57:13 -0400

    all packages

---
+++ public/index.js
@@ -0,0 +1,38 @@
+let deferredPrompt;
+
+const btnApp = document.getElementById("btn-app");
+
+console.log("Hi");
+
+if (!navigator.serviceWorker.controller) {
+  navigator.serviceWorker.register("/sw.js").then(function (reg) {
+    console.log("Service worker has been registered for scope: " + reg.scope);
+  });
+}
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  console.log("Ok we ready");
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+
+  btnApp.style.display = "block";
+});
+
+btnApp.addEventListener("click", handleInstallApp);
+
+function handleInstallApp(e) {
+  btnApp.style.display = "none";
+
+  deferredPrompt.prompt();
+
+  deferredPrompt.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === "accepted") {
+      console.log("User accepted the A2HS prompt");
+    } else {
+      console.log("User dismissed the A2HS prompt");
+    }
+    deferredPrompt = null;
+  });
+}
---
+++ public/sw.js
@@ -0,0 +1,22 @@
+self.addEventListener("install", function (event) {
+  event.waitUntil(preLoad());
+});
+
+var preLoad = function () {
+  console.log("Installing web app");
+};
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    (async function () {
+      try {
+        var res = await fetch(event.request);
+        var cache = await caches.open("cache");
+        cache.put(event.request.url, res.clone());
+        return res;
+      } catch (error) {
+        return caches.match(event.request);
+      }
+    })()
+  );
+});
