2ffe918679d29f5ab499ea969aace7e4b6ffc69f
mostafa.drz <mostafa69d@gmail.com>
2021-07-21 14:00:57 -0400

    add check

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const https = require("https");
-const privateKey = fs.readFileSync("./server.key", "utf8");
-const certificate = fs.readFileSync("./server.crt", "utf8");
 
-const credentials = { key: privateKey, cert: certificate };
 const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
@@ -12,9 +9,21 @@ app.use(express.static("public"));
 
 const PORT = process.env.PORT || 8443;
 // your express configuration here
-const httpsServer = https.createServer(credentials, app);
-
-httpsServer.listen(PORT);
+if (process.env.NODE_ENV === "development") {
+  const privateKey = fs.readFileSync("./server.key", "utf8");
+  const certificate = fs.readFileSync("./server.crt", "utf8");
+  const credentials = { key: privateKey, cert: certificate };
+  const httpsServer = https.createServer(credentials, app);
+  httpsServer.listen(PORT);
+} else {
+  app.listen(PORT, (error) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(`App is running`);
+    }
+  });
+}
 
 app.get("/", (req, res) => {
   res.render("index");
